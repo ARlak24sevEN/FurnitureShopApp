1bda573b3284e27230538e6a4ce3b7b9c5f0cf7d
Arlak <arlak.a@orcsoft.co.th>
2023-04-26 23:24:50 +0700

    finish tabBarView

--- FurnitureShopApp/ContentView.swift
+++ FurnitureShopApp/ContentView.swift
@@ -9,18 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Color("Bg")
+                .ignoresSafeArea()
+            VStack{
+                AppBarView()
+                Text("Fine the Best Furniture")
+            }
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct AppBarView: View {
+    var body: some View {
+        HStack{
+            Image("menu")
+                .padding()
+                .background(Color(.white))
+                .cornerRadius(10)
+            Spacer()
+            Button(action:{}){
+                Image("Profile")
+                    .resizable()
+                    .frame(width: 42, height: 42)
+                    .cornerRadius(10)
+            }
+        }
+        .padding(.horizontal)
     }
 }
