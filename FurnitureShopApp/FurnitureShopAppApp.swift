ce4e55a13514c188e3b823230cf6e3ea04284ac6
Arlak <arlak.a@orcsoft.co.th>
2023-04-26 23:06:27 +0700

    Initial Commit

---
+++ FurnitureShopApp/FurnitureShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureShopAppApp.swift
+//  FurnitureShopApp
+//
+//  Created by Arlak Abdulloh on 26/4/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
