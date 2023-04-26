5b790aab761e4b7094f783b116d9f1f9e167ac59
Arlak <arlak.a@orcsoft.co.th>
2023-04-26 23:07:57 +0700

    test

---
+++ FurnitureShopApp/test.swift
@@ -0,0 +1,20 @@
+//
+//  test.swift
+//  FurnitureShopApp
+//
+//  Created by Arlak Abdulloh on 26/4/2566 BE.
+//
+
+import SwiftUI
+
+struct test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct test_Previews: PreviewProvider {
+    static var previews: some View {
+        test()
+    }
+}
